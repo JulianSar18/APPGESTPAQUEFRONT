df0852de99aeb9b4510be50965d9159b93929418
Julian <juliansarmiento10@outlook.com>
2023-11-09 09:03:45 -0500

    Contiene las APIURL

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+})
+export class Constants {
+
+    public readonly API_URL_PARAMETRIC: string = "http://localhost:8081/api/parametric";
+    public readonly API_URL_SUMMARY: string = "http://localhost:8081/api/summary";
+    public readonly API_URL_TRACKING: string = "http://localhost:8081/api/tracking";
+    public readonly API_URL_OPERATIONAL: string = "http://localhost:8081/api/operational";
+    public readonly API_URL_LIQUIDATOR: string = "http://localhost:8081/api/liquidator";
+
+}
